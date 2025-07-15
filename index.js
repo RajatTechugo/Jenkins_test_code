6594daa4703773cea7aaccf4cb70264266e623c6
Rajat <rajat.paliwal@@techugo.com>
2025-07-15 16:07:49 +0530

    Initial Node.js app

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Hello from Jenkins Node.js app");
+
+let greeting = "Welcome to the Jenkins Node.js application!";
+console.log(greeting);
+
+let currentTime = new Date();
+console.log("Current time is: " + currentTime.toLocaleTimeString());
+
+// Simulating a simple task
+for (let i = 0; i < 5; i++) {
+    console.log("Task iteration: " + (i + 1));
+}
