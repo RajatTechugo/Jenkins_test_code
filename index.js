35e3c49ef035b12436d93e7a0620c63642831623
Rajat <rajat.paliwal@@techugo.com>
2025-07-16 11:55:01 +0530

    index changes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 console.log("Hello from Jenkins Node.js app");
 
 let greeting = "Welcome to the Jenkins Node.js application!";
@@ -6,7 +9,10 @@ console.log(greeting);
 let currentTime = new Date();
 console.log("Current time is: " + currentTime.toLocaleTimeString());
 
-// Simulating a simple task
-for (let i = 0; i < 5; i++) {
-    console.log("Task iteration: " + (i + 1));
-}
+let user = process.env.USER || "Guest";
+console.log(`Hello, ${user}!`);
+console.log(`Running in ${process.env.ENVIRONMENT} environment.`);
+console.log("Environment variables loaded successfully.");
+console.log("Application is running smoothly.");
+console.log("Thank you for using this application!");
+console.log("Goodbye from Jenkins Node.js app!");
